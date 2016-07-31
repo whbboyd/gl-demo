183d560570cc9d922604ea46d4a24e6c399b54f6
Will Boyd <whboyd@wesleyan.edu>
2016-07-31 08:12:35 -0400

    miscellaneous display_math cleanup

--- src/display_math.rs
+++ src/display_math.rs
@@ -13,32 +13,39 @@ pub struct Camera {
 }
 
 pub fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-	let f = {
-		let f = direction;
-		let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-		let len = len.sqrt();
-		[f[0] / len, f[1] / len, f[2] / len]
-	};
-	let s = [up[1] * f[2] - up[2] * f[1],
-			 up[2] * f[0] - up[0] * f[2],
-			 up[0] * f[1] - up[1] * f[0]];
-	let s_norm = {
-		let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-		let len = len.sqrt();
-		[s[0] / len, s[1] / len, s[2] / len]
-	};
-	let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
-			 f[2] * s_norm[0] - f[0] * s_norm[2],
-			 f[0] * s_norm[1] - f[1] * s_norm[0]];
-	let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-			 -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-			 -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
-	[
-		[s[0], u[0], f[0], 0.0],
-		[s[1], u[1], f[1], 0.0],
-		[s[2], u[2], f[2], 0.0],
-		[p[0], p[1], p[2], 1.0],
-	]
+
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s[0], u[0], f[0], 0.0],
+        [s[1], u[1], f[1], 0.0],
+        [s[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+
 }
 
 pub fn perspective_matrix(width: u32, height: u32, fov: f32) -> [[f32; 4]; 4] {
@@ -80,13 +87,8 @@ pub fn handle_mouse_move(window: &Window, camera: &mut Camera, x: i32, y: i32) -
 	// (not really the xy plane; it's the plane determined by xz and [0,1,0])
 	// Clamp dir_y + dy
 	// (the camera will flip if you cross zenith or nadir, which is super confusing)
-	//FIXME: This more-or-less works, but is probably wrong.
+	//FIXME: This more-or-less works, but is probably^Wdefinitely wrong.
 	camera.dir_y += dy as f32 * 0.005;
 
-	let norm_factor = camera.dir_x.abs() + camera.dir_y.abs() + camera.dir_z.abs();
-	camera.dir_x /= norm_factor;
-	camera.dir_y /= norm_factor;
-	camera.dir_z /= norm_factor;
 }
 
-//}
