9d965999cad76737c146b7d9a574a160881c90bb
Will Boyd <whboyd@wesleyan.edu>
2018-05-12 21:12:12 -0400

    Upgrade dependencies, and do the thoroughly staggering amount of refactoring needed to make everything work with new versions

--- src/renderable.rs
+++ src/renderable.rs
@@ -12,7 +12,7 @@ use model::gpu::ModelInstance;
 /// target and renders itself to the target using the state.
 pub trait Renderable<Param, Target> {
 	/// Render this object to the given target using the given state.
-	fn render(&self, renderState: Param, target: Target);
+	fn render(&self, render_state: Param, target: Target);
 }
 
 /// Struct to hold render state for a typical OpenGL 3D object.
@@ -38,31 +38,31 @@ impl<'a> Renderable<&'a DefaultRenderState<'a>, &'a mut Frame> for ModelInstance
 	///
 	/// This computes model/view, model/view/perspective, normal and lighting
 	/// matrices and uses them to 3D render the model instance to the target.
-	fn render(&self, renderstate: &DefaultRenderState, target: &mut Frame) {
-		let light_vector_raw: [f32; 3] = renderstate.light_pos.into();
-		let x: Mat3<f32> = renderstate.view.into();
+	fn render(&self, render_state: &DefaultRenderState, target: &mut Frame) {
+		let light_vector_raw: [f32; 3] = render_state.light_pos.into();
+		let x: Mat3<f32> = render_state.view.into();
 		let light_matrix_raw: [[f32; 3]; 3] = x.into();
-		let model_view = self.model_matrix * renderstate.view;
+		let model_view = self.model_matrix * render_state.view;
 		let model_view_perspective_raw: [[f32; 4]; 4] =
-				(model_view * renderstate.perspective).into();
+				(model_view * render_state.perspective).into();
 		let x: Mat3<f32> = model_view.into();
 		let normal_raw: [[f32; 3]; 3] = x.into();
 		target.draw(
 			&self.model.geometry.vertices,
 			&self.model.geometry.indices,
-			renderstate.program,
+			render_state.program,
 			&uniform! {
 				model_view_perspective_matrix: model_view_perspective_raw,
 				normal_matrix: normal_raw,
 				light_matrix: light_matrix_raw,
 				u_light_pos: light_vector_raw,
-				u_light_color: renderstate.light_color,
+				u_light_color: render_state.light_color,
 				u_mat_ambient: self.model.material.ambient,
 				u_mat_specular: self.model.material.specular,
 				u_mat_texture: self.model.material.texture
 					.sampled().wrap_function(SamplerWrapFunction::Repeat),
 				},
-			renderstate.params).unwrap();
+			render_state.params).unwrap();
 	}
 }
 
